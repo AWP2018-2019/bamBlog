64ec7f8cb8603d179568daa1c206865e6efca075
Marian Belu <belu.marian97@yahoo.com>
2019-01-11 19:27:23 +0000

    Model with migration M

--- app/migrations/0001_initial.py
+++ app/migrations/0001_initial.py
@@ -24,4 +24,14 @@ class Migration(migrations.Migration):
                 ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, related_name='profile', to=settings.AUTH_USER_MODEL)),
             ],
         ),
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('text', models.CharField(max_length=200)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+                ('created_by', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='posts', to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
     ]
--- app/models.py
+++ app/models.py
@@ -11,4 +11,14 @@ User = get_user_model()
 class UserProfile(models.Model):
     user = models.OneToOneField(User, related_name='profile')
     birthday = models.DateField(blank=True)
+    
+
+class Post(models.Model):
+    text = models.CharField(max_length=200)
+    created_by = models.ForeignKey(User, related_name='posts')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{} created by {} at {}".format(self.text, self.created_by.username, self.created_at)
 
