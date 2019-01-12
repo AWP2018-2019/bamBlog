12da7c812315b70f0b4f4cc7db4b6c03cd7788dc
andreisevastre <andrei.sevastre24@gmail.com>
2019-01-12 10:31:31 +0200

    modelele in admin + superuser

--- app/admin.py
+++ app/admin.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+
+from app.models import UserProfile, Post, Comment
+
+admin.site.register(UserProfile)
+admin.site.register(Post)
+admin.site.register(Comment)
+
